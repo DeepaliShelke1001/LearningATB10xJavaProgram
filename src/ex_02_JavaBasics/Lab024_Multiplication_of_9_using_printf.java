cd6d6a623a45375ddfc4353c023e215931d26b97
DeepaliShelke1001 <shelkedeepika1001@gmail.com>
2025-01-18 20:29:32 +0530

    Multiplication of 9 table using print function with new line.

---
+++ src/ex_02_JavaBasics/Lab024_Multiplication_of_9_using_printf.java
@@ -0,0 +1,19 @@
+package ex_02_JavaBasics;
+
+public class Lab024_Multiplication_of_9_using_printf {
+
+    public static void main(String[] args) {
+
+        int d = 9;
+        System.out.print("9*1 = "+d*1+"\n");
+        System.out.print("9*2 = "+d*2+"\n");
+        System.out.print("9*3 = "+d*3+"\n");
+        System.out.print("9*4 = "+d*4+"\n");
+        System.out.print("9*5 = "+d*5+"\n");
+        System.out.print("9*6 = "+d*6+"\n");
+        System.out.print("9*7 = "+d*7+"\n");
+        System.out.print("9*8 = "+d*8+"\n");
+        System.out.print("9*9 = "+d*9+"\n");
+        System.out.print("9*10 = "+d*10+"\n");
+    }
+}
