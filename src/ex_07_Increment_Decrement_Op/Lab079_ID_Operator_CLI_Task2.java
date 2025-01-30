7a9917e4f5055d94a08558aafaf67d8e8ccf991f
DeepaliShelke1001 <shelkedeepika1001@gmail.com>
2025-01-30 23:04:43 +0530

    One file is updated

--- src/ex_07_Increment_Decrement_Op/Lab079_ID_Operator_CLI_Task2.java
+++ src/ex_07_Increment_Decrement_Op/Lab079_ID_Operator_CLI_Task2.java
@@ -5,6 +5,11 @@ public class Lab079_ID_Operator_CLI_Task2 {
 
         // Write a program to Take a user input 2 numbers from the arguments and calculate the maximum in between with ternary operator.
 
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+
+        int largest = (n1 >= n2) ? n1 : n2;
+        System.out.println("The largest number is " + largest);
 
 
     }
